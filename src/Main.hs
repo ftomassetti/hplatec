2fe5c2464d77b03c4744bb1d18bbaf6b8c35f6a2
Federico Tomassetti <f.tomassetti@gmail.com>
2014-10-30 19:13:19 +0000

    Generating a GIF video

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,6 @@ module Main where
 import Foreign
 import Foreign.C.Types
 import Data.Either
-import Data.Either.Unwrap
 import qualified Foreign.Marshal.Array as FMA
 import qualified Data.Map.Strict as M
 import qualified Data.List as L
@@ -14,8 +13,12 @@ import qualified Graphics as G
 import Codec.Picture
 import Codec.Picture.Gif
 
-foreign import ccall "math.h sin"
-     c_sin :: CDouble -> CDouble
+---------------------------------------------------
+-- Bindings
+---------------------------------------------------
+
+foreign import ccall "srand"
+    c_srand :: CUInt -> IO ()
 
 -- | size_t map_side
 -- | float sea_level
@@ -37,6 +40,10 @@ foreign import ccall "platec_api_step"
 foreign import ccall "platec_api_is_finished"
     c_platec_api_is_finished :: Ptr () -> CSize
 
+---------------------------------------------------
+-- Haskell functions
+---------------------------------------------------
+
 toMap :: Int -> [CFloat] -> M.Map (Int,Int) CFloat
 toMap width list = helper list (fromIntegral 0) (fromIntegral 0) M.empty
                    where helper :: [CFloat] -> Int -> Int -> M.Map (Int,Int) CFloat -> M.Map (Int,Int) CFloat
@@ -54,30 +61,40 @@ saveMap p filename = do let heightmap = c_platec_api_get_heightmap p
 getFrame p = do
     let heightmap = c_platec_api_get_heightmap p
     heightmap' :: [CFloat] <- FMA.peekArray (512*512) heightmap
-    putStrLn $ show (head heightmap')
     let heightmap'' = G.MakeMap 512 512 (toMap 512 heightmap')
     let img = G.generateGifMap heightmap''
     return img
 
 writeAnimation filename frames = do writeGifImages filename LoopingForever frames'
-                                    where frames' = L.map (\f -> (greyPalette, 35, f)) frames
+                                    where frames' = L.map (\f -> (greyPalette, 20, f)) frames
 
-getFrames p = helper p []
-              where helper :: Ptr () -> [Image Pixel8] -> IO [Image Pixel8]
-                    helper p acc = if (c_platec_api_is_finished p) /= 0 || (length acc > 100)
-                                   then return acc
-                                   else do c_platec_api_step p
-                                           frame :: Image Pixel8 <- getFrame p
-                                           putStrLn $ "Getting frame " ++ (show (c_platec_api_is_finished p)) ++ " frames " ++ (show $ length acc)
-                                           helper p (acc ++ [frame])
+getFrames p = helper p [] 0
+              where helper :: Ptr () -> [Image Pixel8] -> Int -> IO [Image Pixel8]
+                    helper p acc i = if (c_platec_api_is_finished p) /= 0 || (length acc > 200)
+                                     then return acc
+                                     else do
+                                        c_platec_api_step p
+                                        acc' <- if i == 0
+                                                then do
+                                                    frame :: Image Pixel8 <- getFrame p
+                                                    putStrLn $ "Getting frame " ++ (show $ length acc)
+                                                    return $ acc ++ [frame]
+                                                else return acc
+                                        helper p acc' ((i+1) `mod` 10)
 
-processRes (Data.Either.Left msg) = do putStrLn msg
-processRes (Right res) = do res
-                            putStrLn "DONE"
+processRes (Data.Either.Left msg) = do
+    putStrLn "Error while producing the animation:"
+    putStrLn msg
 
-main = do let s = c_sin 10.5
-          p <- c_platec_create 512 0.65 60 0.02 1000000 0.33 2 10
-          frames <- getFrames p
-          let res = writeAnimation "animation2.gif" frames
-          processRes res
-          return ()
+processRes (Right res) = do
+    res
+    putStrLn "DONE"
+
+main = do
+    c_srand 1
+    p <- c_platec_create 512 0.65 60 0.02 1000000 0.33 2 10
+    frames <- getFrames p
+    putStrLn "Frames produced, saving animation (it could take a while)"
+    let res = writeAnimation "animation3.gif" frames
+    processRes res
+    return ()
